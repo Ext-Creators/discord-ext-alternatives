042e045a3b7f4ed422a5b8bf40499dd349456883
Ben Simms <ben@bensimms.moe>
2020-08-03 01:45:58 +0100

    Fix typing.Literal converter (#14)
    * Add missing import to literal_converter
    
    * Actually fix typing.Literal
    
    * Fix error case
    
    * Make use of typing.get_origin

--- discord/ext/alternatives/literal_converter.py
+++ discord/ext/alternatives/literal_converter.py
@@ -1,23 +1,26 @@
-from typing import Literal, get_args
-from discord.ext.commands.errors import ConversionError
+from typing import Literal, get_args, get_origin
+from discord.ext.commands import Command
+from discord.ext.commands.errors import ConversionError, BadArgument
 
 _old_actual_conversion = Command._actual_conversion
 
-def _actual_conversion(self, ctx, converter, argument, param):
-    if converter is Literal:
+async def _actual_conversion(self, ctx, converter, argument, param):
+    origin = get_origin(converter)
+
+    if origin is Literal:
         items = get_args(converter)
 
         if all(i for i in items if isinstance(i, str)):
             if argument in items:
                 return argument
                 
-            raise commands.BadArgument(f"Expected literal: one of {list(map(repr, self.literals))}")
+            raise BadArgument(f"Expected literal: one of {list(map(repr, items))}")
         elif all(i for i in items if not isinstance(i, str)):
-            ret = _old_actual_conversion(self, ctx, type(items[0]), argument, param)
+            ret = await _old_actual_conversion(self, ctx, type(items[0]), argument, param)
             return ret in items
         else:
             raise ConversionError('Literal contains multiple conflicting types.')
     
-    return _old_actual_conversion(self, ctx, converter, argument, param)
+    return await _old_actual_conversion(self, ctx, converter, argument, param)
 
 Command._actual_conversion = _actual_conversion
