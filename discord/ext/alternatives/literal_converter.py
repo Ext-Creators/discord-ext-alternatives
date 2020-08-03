b163ffbaa63b833b551bfa961ad6b6e03a639b50
Ben Simms <ben@bensimms.moe>
2020-08-03 01:26:02 +0100

    Actually fix typing.Literal

--- discord/ext/alternatives/literal_converter.py
+++ discord/ext/alternatives/literal_converter.py
@@ -4,8 +4,13 @@ from discord.ext.commands.errors import ConversionError
 
 _old_actual_conversion = Command._actual_conversion
 
-def _actual_conversion(self, ctx, converter, argument, param):
-    if converter is Literal:
+async def _actual_conversion(self, ctx, converter, argument, param):
+    try:
+        origin = converter.__origin__
+    except AttributeError:
+        origin = None
+
+    if origin is Literal:
         items = get_args(converter)
 
         if all(i for i in items if isinstance(i, str)):
@@ -14,11 +19,11 @@ def _actual_conversion(self, ctx, converter, argument, param):
                 
             raise commands.BadArgument(f"Expected literal: one of {list(map(repr, self.literals))}")
         elif all(i for i in items if not isinstance(i, str)):
-            ret = _old_actual_conversion(self, ctx, type(items[0]), argument, param)
+            ret = await _old_actual_conversion(self, ctx, type(items[0]), argument, param)
             return ret in items
         else:
             raise ConversionError('Literal contains multiple conflicting types.')
     
-    return _old_actual_conversion(self, ctx, converter, argument, param)
+    return await _old_actual_conversion(self, ctx, converter, argument, param)
 
 Command._actual_conversion = _actual_conversion
