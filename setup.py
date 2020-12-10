fdafcf4ad5bc5134512d759ef871a53c750a3877
Riley Shaw <contact@shiney.dev>
2020-12-10 19:45:55 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,52 @@
-from setuptools import setup
 import re
+import setuptools
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
 
-version = ""
-with open("discord/ext/alternatives/__init__.py") as f:
-    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
+with open("README.rst", "r") as stream:
+    long_description = stream.read()
 
-with open("README.rst", encoding="utf-8") as f:
-    readme = f.read()
+with open("requirements.txt") as stream:
+    install_requires = stream.read().splitlines()
 
-setup(
+with open("discord/ext/alternatives/__init__.py") as stream:
+    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", stream.read(), re.MULTILINE).group(1)
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+project_urls = {
+    "Issue Tracker": "https://github.com/Ext-Creators/discord-ext-alternatives/issues",
+    "Source": "https://github.com/Ext-Creators/discord-ext-alternatives",
+}
+
+setuptools.setup(
+    author="Ext-Creators",
+    classifiers=classifiers,
+    description="A discord.py extension with additional and alternative features.",
+    install_requires=install_requires,
+    license="Apache Software License",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     name="discord-ext-alternatives",
-    author="NCPlayz",
+    packages=["discord.ext.alternatives"],
+    project_urls=project_urls,
     python_requires=">=3.5.3",
     url="https://github.com/Ext-Creators/discord-ext-alternatives",
     version=version,
-    packages=["discord.ext.alternatives"],
-    license="Apache Software License",
-    description="Patches some additional/alternative features into discord.py.",
-    long_description=readme,
-    long_description_content_type="text/x-rst",
-    include_package_data=True,
-    install_requires=requirements,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Internet",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-    ]
 )
