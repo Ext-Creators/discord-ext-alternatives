b966561c591f5413a3fd2045a3f6fc51dc43dd6c
Steve C <diceroll123@gmail.com>
2021-04-13 01:09:56 -0400

    Add role.py which implements "shaving" members of the role.

--- discord/ext/alternatives/int_map.py
+++ discord/ext/alternatives/int_map.py
@@ -28,6 +28,7 @@ discord.Attachment.__int__ = _int
 discord.AuditLogEntry.__int__ = _int
 discord.emoji._EmojiTag.__int__ = _int
 discord.mixins.Hashable.__int__ = _int
+discord.Member.__int__ = _int
 discord.Message.__int__ = _int
 discord.Reaction.__int__ = _int
 discord.Team.__int__ = _int
