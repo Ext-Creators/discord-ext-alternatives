aba29cb26a664bab32745ee937d5fb650e2521dc
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-04-16 12:49:01 +0100

    update setup

--- setup.py
+++ setup.py
@@ -2,15 +2,6 @@ import re
 import setuptools
 
 
-with open("README.rst", "r") as stream:
-    long_description = stream.read()
-
-with open("requirements.txt") as stream:
-    install_requires = stream.read().splitlines()
-
-with open("discord/ext/alternatives/__init__.py") as stream:
-    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", stream.read(), re.MULTILINE).group(1)
-
 classifiers = [
     "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
@@ -30,19 +21,52 @@ classifiers = [
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
+with open("requirements.txt") as stream:
+    install_requires = stream.read().splitlines()
+
+packages = [
+    "discord.ext.alternatives",
+]
+
 project_urls = {
     "Issue Tracker": "https://github.com/Ext-Creators/discord-ext-alternatives/issues",
     "Source": "https://github.com/Ext-Creators/discord-ext-alternatives",
 }
 
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("discord/ext/alternatives/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+with open("discord/ext/alternatives/__init__.py") as stream:
+    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", stream.read(), re.MULTILINE).group(1)
+
 setuptools.setup(
     author="Ext-Creators",
     classifiers=classifiers,
-    description="A discord.py extension with additional and alternative features. Use with caution.",
+    description="A discord.py extension with additional and alternative features.",
     install_requires=install_requires,
     license="Apache Software License",
     name="discord-ext-alternatives",
-    packages=["discord.ext.alternatives"],
+    packages=packages,
     project_urls=project_urls,
     python_requires=">=3.5.3",
     url="https://github.com/Ext-Creators/discord-ext-alternatives",
