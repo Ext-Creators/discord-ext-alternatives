2987112c31de9bada4ee95cee9007a326e496e19
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-04-26 15:55:53 +0100

    update init

--- discord/ext/alternatives/__init__.py
+++ discord/ext/alternatives/__init__.py
@@ -3,5 +3,5 @@ import collections
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "year month day release serial")
 
+version = "2021.4.13"
 version_info = _VersionInfo(2021, 4, 13, "final", 0)
-version = "2021.04.13"
